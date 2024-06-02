ec989e3bef419137dfaadb66bad2329eaf4eaa76
EC2 Default User <ec2-user@ip-172-31-9-81.ec2.internal>
2024-06-02 21:27:30 +0000

    Deleted previous app

--- frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure({
-    Auth: {
-        region: 'us-east-1',
-        userPoolId: 'us-east-1_lOLYXthG0',
-        userPoolWebClientId: '2u7ggm9t0934c27h39bhci9h03'
-    }
-});
